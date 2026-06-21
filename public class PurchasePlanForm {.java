0540cfb38b170c6f3b0eb0443cda2959855c850f
ishimwe-gentil <igentilherve@gmail.com>
2026-06-21 12:27:55 -0400

    Initializing PurchasePlanForm java class

---
+++ public class PurchasePlanForm {.java
@@ -0,0 +1,17 @@
+public class PurchasePlanForm {
+    private String purchaseItem;
+    private double purchaseAmt;
+    private String purchasePrior;
+    private LocalDate planDate;
+    private boolean purchaseDone;
+
+    public PurchasePlanForm(String purchaseItem, double purchaseAmt, String purchasePrior, LocalDate planDate) {
+        this.purchaseItem = buyItem;
+        this.purchaseAmt = buyAmt;
+        this.purchasePrior = buyPrior;
+        this.planDate = buyDate;
+        this.purchaseDone = false;
+    }
+
+}
+ 
