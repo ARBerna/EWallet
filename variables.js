c8a5d2a08ad71b874afe87d44f15665a8c68ade3
Ashley Berna <arbgab2020@gmail.com>
2026-06-28 01:27:16 +0000

    finished variable.js to hold and update the variables for the app

---
+++ variables.js
@@ -0,0 +1,9 @@
+export const appState = {
+    balance: 0,
+    transactions: [],
+    income: 0,
+    expenses: 0
+};
+
+//use import on each js file that uses this:
+//import { appState } from './variables.js';
